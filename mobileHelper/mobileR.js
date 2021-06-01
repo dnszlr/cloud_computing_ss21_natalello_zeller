e1324f5c251457b4533dc8d62f4ce9edacf9d374
dennis <Dennis.Zhukki@gmail.com>
2021-06-01 16:05:36 +0200

    fixed sendPayment

--- mobileHelper/mobileR.js
+++ mobileHelper/mobileR.js
@@ -4,7 +4,7 @@ const router = express.Router();
 let controller = require('./mobileC');
 
 router.post('/checkout', controller.receivePayment);
-router.get('/send', controller.sendPayment);
+router.post('/send', controller.sendPayment);
 router.post("/refund", controller.refundPayment);
 router.get('/createuser', controller.createUser);
 router.get('/clienttoken', controller.getClientToken);
