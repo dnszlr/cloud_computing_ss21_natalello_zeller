bec6cc56d5c789e001bf22635c31b35c8fd4b06a
dennis <Dennis.Zhukki@gmail.com>
2021-06-15 16:55:33 +0200

    redislab tryout

--- config/config.js
+++ config/config.js
@@ -14,6 +14,12 @@ const config = {
     },
     cookie: {
         lifetime: 1800000
+    },
+    redis: {
+        host: 'redis-18403.c1.eu-west-1-3.ec2.cloud.redislabs.com',
+        port: 18403,
+        accessControl: 'User',
+        password: '28aZNQvjg66wHCzmIoxYzEcvOczwc1Lf'
     }
 };
 
